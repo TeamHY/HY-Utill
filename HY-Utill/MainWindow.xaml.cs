4fe318d582c8e0b27dae89e75af9b3feaec28f38
Pdom <ahb0327@naver.com>
2017-09-21 17:40:45 +0900

    Change main window

--- HY-Utill/MainWindow.xaml.cs
+++ HY-Utill/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace HY_Utill
 {
@@ -19,9 +22,151 @@ namespace HY_Utill
     /// </summary>
     public partial class MainWindow : Window
     {
+        private delegate void CSafeSetMaximum(Int32 value);
+        private delegate void CSafeSetValue(Int32 value);
+
+        private CSafeSetMaximum cssm;
+        private CSafeSetValue cssv;
+
+        private string latestVersion;
+
+        private WebClient wc;
+        private bool setBaseSize;
+        private bool nowDownloading;
+
         public MainWindow()
         {
+            cssm = new CSafeSetMaximum(CrossSafeSetMaximumMethod);
+            cssv = new CSafeSetValue(CrossSafeSetValueMethod);
+
+            wc = new WebClient();
+
             InitializeComponent();
+        }
+
+        private void CheckVersion()
+        {
+            string storageUrl = @"https://teamhy.github.io/";
+
+            System.IO.Path.GetTempFileName();
+
+            XmlDocument currentXml = new XmlDocument();
+            XmlDocument latestXml = new XmlDocument();
+
+            //currentXml.Load("카그 경로");
+            latestXml.Load(storageUrl + "versiondata.xml");
+
+            XmlNodeList xnList = latestXml.SelectNodes("/versiondata/chaosgreedier");
+
+            foreach (XmlNode xn in xnList)
+            {
+                //string changelog = xn["changelog"].InnerText;
+
+                latestVersion = xn["version"].InnerText;
+                lblLatestVersion.Content = latestVersion;
+
+                StartFileDownload(String.Format(@"https://teamhy.github.io/ChaosGreedier_{0}.7z", latestVersion), Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\Binding of Isaac Afterbirth+ Mods");
+            }
+        }
+
+        private void StartFileDownload(String remoteAddress, String localPath)
+        {
+            if (nowDownloading)
+            {
+                MessageBox.Show("이미 다운로드가 진행 중입니다.", "오류", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(remoteAddress))
+            {
+                MessageBox.Show("주소가 입력되지 않았습니다.", "오류", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            // 파일이 저장될 위치를 저장한다.
+            String fileName = String.Format(@"{0}\{1}", localPath, System.IO.Path.GetFileName(remoteAddress));
+
+            // 폴더가 존재하지 않는다면 폴더를 생성한다.
+            if (!System.IO.Directory.Exists(localPath))
+                System.IO.Directory.CreateDirectory(localPath);
+
+            try
+            {
+                // C 드라이브 밑의 downloadFiles 폴더에 파일 이름대로 저장한다.
+                wc.DownloadFileAsync(new Uri(remoteAddress), fileName);
+
+                // 다운로드 중이라는걸 알리기 위한 값을 설정하고,
+                // 프로그레스바의 크기를 0으로 만든다.
+                prgInstall.Value = 0;
+                prgInstall.Foreground = Brushes.Gold;
+                setBaseSize = false;
+                nowDownloading = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().FullName, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void CrossSafeSetMaximumMethod(Int32 value)
+        {
+            if (!prgInstall.Dispatcher.CheckAccess())
+                prgInstall.Dispatcher.Invoke(cssm, value);
+            else
+                prgInstall.Maximum = value;
+        }
+        private void CrossSafeSetValueMethod(Int32 value)
+        {
+            if (!prgInstall.Dispatcher.CheckAccess())
+                prgInstall.Dispatcher.Invoke(cssm, value);
+            else
+                prgInstall.Value = value;
+        }
+
+        private void MainFormLoad(object sender, RoutedEventArgs e)
+        {
+
+            // 이벤트를 연결한다.
+            wc.DownloadFileCompleted += new AsyncCompletedEventHandler(FileDownloadCompleted);
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(FileDownloadProgressChanged);
+
+        }
+
+        void FileDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+            // e.BytesReceived
+            //   받은 데이터의 크기를 저장합니다.
+
+            // e.TotalBytesToReceive
+            //   받아야 할 모든 데이터의 크기를 저장합니다.
+
+            // 프로그레스바의 최대 크기가 정해지지 않은 경우,
+            // 받아야 할 최대 데이터 량으로 설정한다.
+            if (!setBaseSize)
+            {
+                CrossSafeSetMaximumMethod((int)e.TotalBytesToReceive);
+                setBaseSize = true;
+            }
+
+            // 받은 데이터 량을 나타낸다.
+            CrossSafeSetValueMethod((int)e.BytesReceived);
+
+            // 받은 데이터 / 받아야할 데이터 (퍼센트) 로 나타낸다.
+            //CrossSafeSetTextMethod(String.Format("{0:N0} / {1:N0} ({2:P})", e.BytesReceived, e.TotalBytesToReceive, (Double)e.BytesReceived / (Double)e.TotalBytesToReceive));
+        }
+
+        void FileDownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            nowDownloading = false;
+            //btnStart.Enabled = true;
+            //txtAddress.Enabled = true;
+            MessageBox.Show("파일 다운로드 완료!", "오류", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CheckVersion();
         }
     }
 }
